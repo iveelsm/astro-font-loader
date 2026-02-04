0db5bd7479a0ab8aa7d2333012c898829a30215a
Mikey Sleevi <development@frogman.simplelogin.com>
2026-02-04 13:28:27 -0700

    Add support for retrieving available fonts

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,6 +1,7 @@
 import type { FontsPackageInfo } from "./fontInfo.js";
 import { getFontsPackageInfo } from "./package.js";
+import { getAvailableFonts } from "./available.ts";
 
 export type { FontsPackageInfo };
 
-export { getFontsPackageInfo };
+export { getFontsPackageInfo, getAvailableFonts };
