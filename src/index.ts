65bfae7ffe52b98b862a43c7964a92c7fe11931d
Mikey Sleevi <development@frogman.simplelogin.com>
2026-02-06 12:59:02 -0700

    fix: Cleaning up the package for imports, exports and configuration options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { getFontsCss } from "./css/index.ts";
 export { fontsIntegration } from "./integration.ts";
-export { getFontsPackageInfo, getAvailableFonts } from "./fonts/index.ts";
 export type { FontsIntegrationOptions } from "./integration.ts";
+
+export { getFontsCss } from "./css/index.ts";
+export type { GetFontsCssOptions } from "./css/get.ts";
+
+export { getFontsPackageInfo, getAvailableFonts } from "./fonts/index.ts";
+export type { FontInfo, FontsPackageInfo } from "./fonts/index.ts";
