71e8a611b52c8c0dd44e37edada70c4bd5019242
Mikey Sleevi <development@frogman.simplelogin.com>
2026-05-25 10:35:15 -0600

    reduce scope of interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export { fontsIntegration } from "./integration.ts";
 
-export { getFontsCss } from "./css/index.ts";
-export type { GetFontsCssOptions } from "./css/get.ts";
-
-export { getFontsPackageInfo, getAvailableFonts, createVariantFilter } from "./fonts/index.ts";
-export type { FontInfo, FontsPackageInfo } from "./fonts/index.ts";
-
 export type { FontLoaderConfig, FontConfig, FontSource, PackageProvider, FontVariant } from "./types.ts";
