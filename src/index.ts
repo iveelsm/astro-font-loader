05b2792372b8e8115b313023a269ed4816a16b21
Mikey Sleevi <development@frogman.simplelogin.com>
2026-05-26 09:18:24 -0600

    fix: Only use exported functions (#117)
    The component cannot resolve the local files at build time, so export the necessary functions and utilize them from the surfaced package interface.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { fontsIntegration } from "./integration.ts";
+export { getFontsCss } from "./css/index.ts";
+export { getFontsPackageInfo } from "./fonts/index.ts";
 
 export type { FontLoaderConfig, FontConfig, FontSource, PackageProvider, FontVariant, PreloadEntry } from "./types.ts";
